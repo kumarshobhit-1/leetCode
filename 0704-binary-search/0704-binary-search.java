19a42d43b6638f6ae50ee08ec07fcba803858733
Shobhit Kumar <146829121+kumarshobhit-1@users.noreply.github.com>
2026-06-22 23:54:43 +0530

    Time: 1 ms (9.97%), Space: 48.2 MB (68.32%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.java
@@ -0,0 +1,16 @@
+class Solution {
+    public static int search(int[] nums, int target) {
+        for(int i = 0; i<nums.length; i++){
+            if(nums[i] == target){
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args){
+        int nums[] = {-1,0, 3, 5, 9, 12};
+        int target = 4;
+        int result = search(nums, target);
+        System.out.println(result);
+    }
+}
